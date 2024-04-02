6fa7fc188db77e8a08ae30fb1a7d7183590dbcb3
Matthew Ashley <z10@outlook.com.au>
2024-04-02 19:56:38 +1100

    initial push of basic input

--- src/app/(protected)/search/page.tsx
+++ src/app/(protected)/search/page.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from "next";
-
+import {
+  SearchIcon,
+} from "lucide-react";
+import { Input } from "@/components/ui/input"
 export const metadata: Metadata = {
   title: "Search",
   description: "",
@@ -8,7 +11,14 @@ export const metadata: Metadata = {
 const SearchPage = () => {
   return (
     <main className="padding top-margin flex-1 flex flex-col gap-5">
-      <h1>Search</h1>
+      
+      <h1>Search SME</h1>
+      <Input type = "name" placeholder = "Search for highgrowth SME's" />
+      <button><SearchIcon></SearchIcon></button>
+      <table>
+        <text>test search</text>
+      </table>
+      
     </main>
   );
 };
