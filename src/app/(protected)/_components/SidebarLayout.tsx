f0e63fef560429424db30308fa874b7653db92ae
fadi-gorges <Fadi.Gorges@student.uts.edu.au>
2024-05-08 03:47:13 +1000

    Fix SidebarLayout bug with 2 scrollbars

--- src/app/(protected)/_components/SidebarLayout.tsx
+++ src/app/(protected)/_components/SidebarLayout.tsx
@@ -5,10 +5,12 @@ import React from "react";
 const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex-1 grid grid-cols-12">
-      <Sidebar />
+      <div className="lg:col-span-3 2xl:col-span-2">
+        <Sidebar />
+      </div>
       <div
         id="page-div"
-        className="col-span-12 lg:col-span-9 2xl:col-span-10 flex flex-col max-h-[calc(100vh-64px)] overflow-auto"
+        className="col-span-12 lg:col-span-9 2xl:col-span-10 flex flex-col"
       >
         {children}
       </div>
