bda8686f54c3d2098a217601d6919a9ef0bdd0a3
fadi-gorges <Fadi.Gorges@student.uts.edu.au>
2024-05-08 03:59:57 +1000

    Fix SidebarLayout page bug on mobile

--- src/app/(protected)/_components/SidebarLayout.tsx
+++ src/app/(protected)/_components/SidebarLayout.tsx
@@ -5,9 +5,7 @@ import React from "react";
 const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex-1 grid grid-cols-12">
-      <div className="lg:col-span-3 2xl:col-span-2">
-        <Sidebar />
-      </div>
+      <Sidebar />
       <div
         id="page-div"
         className="col-span-12 lg:col-span-9 2xl:col-span-10 flex flex-col"
