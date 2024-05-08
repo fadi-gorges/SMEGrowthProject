42d000fb01c91d81f8158a343c310d022424b7fd
Pet3rP <piradon291@gmail.com>
2024-05-08 15:40:53 +1000

    Name Change

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -28,7 +28,7 @@ const DashboardPage = async () => {
 
   return (
       <div className="p-5 w-full">
-      <h1 className="text-3xl font-bold mb-4">Organisation Dashboard</h1>
+      <h1 className="text-3xl font-bold mb-4">Businesses Recommended</h1>
       <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left text-gray-500">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50">
